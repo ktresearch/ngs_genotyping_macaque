81b4a90c0547f94848ec0eb4a552c856bc48101e
ktresearch <54824730+ktresearch@users.noreply.github.com>
2020-10-20 17:25:12 +0900

    Add files via upload

---
+++ MHC_genotyping/pipeline/MHC_pipeline_v5.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+read -p "input Fastqfile:" fastqfile
+read -p "input Mamu or Mafa:" species
+read -p "input runid (any characters):" runid
+read -p "input barcodeid (any characters):" barcodeid
+read -p "input samplename (any characters):" sampleid
+
+length=150
+qv=20
+percent=90
+
+if [ $species = "Mamu" ]; then
+  blast_reference_fasta=./reference/190312_Mamu_classI_Amplifiable_ver2_Realigned_Include_Rev1.fasta
+else
+  blast_reference_fasta=./reference/190304_Mafa_classI_Amplifiable_ver2_Realigned_Include_Rev1.fasta
+fi
+
+cutadapt -b GTGGGCTACGTGGACGAC $fastqfile > $runid"_"$barcodeid"_removeFp.fastq"
+cutadapt -b GTCGTCCACGTAGCCCAC $runid"_"$barcodeid"_removeFp.fastq" > $runid"_"$barcodeid"_removeFpm.fastq"
+
+cutadapt -b CTTCTACCCTGCGGAGATCA $runid"_"$barcodeid"_removeFpm.fastq" > $runid"_"$barcodeid"_removeFRp.fastq"
+cutadapt -b TGATCTCCGCAGGGTAGAAG $runid"_"$barcodeid"_removeFRp.fastq" > $runid"_"$barcodeid"_removeFR.fastq"
+
+perl pipeline/fastq_remove_readlength.pl $length $runid"_"$barcodeid"_removeFR.fastq" $runid"_"$barcodeid"_"$length"bp.fastq"
+
+perl pipeline/fastq_remove_quality.pl 20 90 $runid"_"$barcodeid"_"$length"bp.fastq" $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent".fastq"
+
+perl pipeline/fastq2fasta.pl $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent".fastq" $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent".fasta"
+
+blastn -db $blast_reference_fasta -query $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent".fasta" -evalue 1e-10 -perc_identity 98 -max_target_seqs 10000 -outfmt 6 -num_threads 4 -out $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent"_blast_eval1e10_perc98.txt"
+
+python3 pipeline/count_multihit_v5.py $length $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent"_blast_eval1e10_perc98.txt" $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent".fasta" $species
+
+python3 pipeline/count_uniquehit_v5.py $length $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent"_blast_eval1e10_perc98.txt" $runid"_"$barcodeid"_"$length"bp_q"$qv"p"$percent".fasta" "count_multihit_"$length".txt" $species
